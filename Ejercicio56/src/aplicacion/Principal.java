f45520285d834a6491d6baded0a2be0bb88bb16b
rubencerroo <ruben.cerrodiaz@usp.ceu.es>
2022-12-17 16:53:11 +0100

    56

---
+++ Ejercicio56/src/aplicacion/Principal.java
@@ -0,0 +1,125 @@
+package aplicacion;
+import dominio.Contacto;
+import dominio.Agenda;
+
+import java.util.*;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+       
+        boolean salir = false;
+        int opcion; //Guardaremos la opcion del usuario
+
+        Agenda agendaTelefonica = new Agenda(3);
+        String nombre;
+        int telefono;
+
+        Contacto c;
+
+        while (!salir) {
+
+            System.out.println("1. Añadir contacto");
+            System.out.println("2. Listar contactos");
+            System.out.println("3. Buscar contacto");
+            System.out.println("4. Existe contacto");
+            System.out.println("5. Eliminar contacto");
+            System.out.println("6. Contactos disponibles");
+            System.out.println("7. Agenda llena");
+            System.out.println("8. Salir");
+            try {
+                System.out.println("Escribe una de las opciones");
+                opcion = sc.nextInt(); 
+
+                switch (opcion) {
+                    case 1 :
+
+                        //Pido valores
+                        System.out.println("Escribe un nombre");
+                        nombre = sc.next();
+
+                        System.out.println("Escribe un telefono");
+                        telefono = sc.nextInt();
+
+                        //Creo el contacto
+                        c = new Contacto(nombre, telefono);
+
+                        agendaTelefonica.aniadirContacto(c);
+
+                        break;
+                    case 2:
+
+                        agendaTelefonica.listarContactos();
+
+                        break;
+                    case 3:
+
+                        //pido el nombre
+                        System.out.println("Escribe un nombre");
+                        nombre = sc.next();
+
+                        agendaTelefonica.buscarPorNombre(nombre);
+
+                        break;
+                    case 4:
+
+                        //pido el nombre
+                        System.out.println("Escribe un nombre");
+                        nombre = sc.next();
+
+                        //Creo el contacto auxiliar
+                        c = new Contacto(nombre, 0);
+
+                        if (agendaTelefonica.existeContacto(c)) {
+                            System.out.println("Existe contacto");
+                        } else {
+                            System.out.println("No existe contacto");
+                        }
+
+                        break;
+                    case 5:
+
+                        //pido el nombre
+                        System.out.println("Escribe un nombre");
+                        nombre = sc.next();
+
+                        //Creo el contacto auxiliar
+                        c = new Contacto(nombre, 0);
+
+                        agendaTelefonica.eliminarContacto(c);
+
+                        break;
+                    case 6:
+
+                        System.out.println("Hay " + agendaTelefonica.huecosLibre() + " contactos libre");
+
+                        break;
+                    case 7:
+
+                        //como devuelve un booleano, lo meto en un if
+                        if (agendaTelefonica.agendaLlena()) {
+                            System.out.println("La agenda esta llena");
+                        } else {
+                            System.out.println("Aun puedes meter contactoss");
+                        }
+
+                        break;
+                    case 8:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Solo números entre 1 y 8");
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Debes insertar un número");
+                sc.next();
+            }
+
+        }
+
+    }
+
+}
